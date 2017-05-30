4b99f123ca5792536caf639f04a1836914fd5b45
Stytan <stytanus@gmail.com>
2017-05-30 09:04:47 +0300

    Update Program.cs

--- DZ_6_6/Program.cs
+++ DZ_6_6/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DZ_6_6
 {
@@ -10,7 +7,41 @@ namespace DZ_6_6
     {
         static void Main(string[] args)
         {
-
+			Console.WriteLine("Counting the number of identical words in the text");
+			Console.WriteLine("Enter text: ");
+			string text = Console.ReadLine();
+			string[] buf = text.Split(",. ;:!?".ToCharArray());
+			var dict = new Dictionary<string,int>();
+			if(buf.Length==0)
+			{
+				Console.WriteLine("Not found a words in text");
+			}
+			else
+			{
+				foreach(string str in buf)
+				{
+					if (str != "") {
+						try {
+							dict.Add(str, 1);
+						} catch (ArgumentNullException) {
+							continue;
+						} catch (ArgumentException) {
+							//такой ключ уже есть в коллекции - добавляем ему значение
+							dict[str]++;
+						}
+					}
+				}
+				if(dict.Count>0)
+				{
+					var dictEnum = dict.GetEnumerator();
+					while(dictEnum.MoveNext())
+					{
+						Console.WriteLine(dictEnum.Current.Key + " - " + dictEnum.Current.Value);
+					}
+				}
+			}
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
 }
